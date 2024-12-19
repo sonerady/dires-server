2fdb3250ec421ea215204430e1b7044a39b8dac3
Soner <80179760+sonerady@users.noreply.github.com>
2024-12-20 01:07:58 +0300

    fixed

--- src/routes/generateTrain.js
+++ src/routes/generateTrain.js
@@ -437,7 +437,7 @@ router.post("/generateTrain", upload.array("files", 20), async (req, res) => {
                 destination: `appdiress/${repoName}`,
                 input: {
                   steps: 1000,
-                  lora_rank: 30,
+                  lora_rank: 16,
                   optimizer: "adamw8bit",
                   batch_size: 1,
                   resolution: "512,768,1024",
