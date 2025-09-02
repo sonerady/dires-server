0ea8ec6b96f42d85ba3f4ecdde4f87a8e99d14fb
Soner <80179760+sonerady@users.noreply.github.com>
2025-09-02 04:55:14 +0300

    fixed

--- src/routes/favoritesRoutes.js
+++ src/routes/favoritesRoutes.js
@@ -94,7 +94,7 @@ router.get("/:userId", async (req, res) => {
         location_image_url: location?.image_url || "",
         location_category: location?.category || favorite.location_category,
         location_type: location?.location_type || favorite.location_type,
-        favorite_count: typeof location?.favorite_count === "number" ? location.favorite_count : 0,
+        favorite_count: location?.favorite_count || 0,
       };
     });
 
